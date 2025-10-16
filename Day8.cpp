a59409d421782a5130bd8816b80ca7366db3a120
Ram Chandra Gupta <mrchandra142@gmail.com>
2025-10-16 16:48:43 +0530

    Adding file to day 08

--- Day8.cpp
+++ Day8.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n = 5;
-    for(int i = 0;i<n; i++){
-        for(int j = 0;j <n; j++){
-            cout<<"*";
-        }
-        cout<<endl;
+
+void binToDec(int binnum){
+    int n= binnum;
+    int decnum = 0;
+    int pow = 1;
+    while(n>0){
+        int lsd = n%10;
+        decnum += lsd * pow; 
+        pow = pow * 2;
+        n/=10;
     }
+    cout<<decnum;
+
 }
+
+void DecToBin(int decnum){
+    int n = decnum;
+    int binnum  = 0;
+    int pow = 1;
+    while(n>0){
+        int rem = n%2;
+        binnum  = binnum + rem*pow;
+        pow = pow*10;
+        n/=2;
+    }
+    cout<<binnum;
+}
+int main(){
+    binToDec(1001);
+    DecToBin(16);
+}
+
